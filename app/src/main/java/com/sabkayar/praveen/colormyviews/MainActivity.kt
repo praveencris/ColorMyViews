74e46b7546d7580ea35eb722ee9fc54b598ad102
Praveen <pkchmits@gmail.com>
2020-09-20 03:51:32 +0530

    Learnt about chains- spread,spread-internal,packed Kotlin coding for setting up listener Most functionalities of constraint layout

--- app/src/main/java/com/sabkayar/praveen/colormyviews/MainActivity.kt
+++ app/src/main/java/com/sabkayar/praveen/colormyviews/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.sabkayar.praveen.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sabkayar.praveen.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(
+                mBinding.boxOneText, mBinding.boxTwoText, mBinding.boxThreeText
+                , mBinding.boxFourText, mBinding.boxFiveText,mBinding.constraintLayout
+            )
+        for (item in clickableViews)
+            item.setOnClickListener() {
+                makeColored(it)
+            }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
